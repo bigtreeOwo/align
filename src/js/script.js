19e5f38d6ae5a26f840c5ce30f4352e971a5bbb7
bigtree <zz1z2zz3z4zz5@qq.com>
2023-04-16 00:44:19 +0800

    第六次提交，主要添加了内嵌在线blast界面,readme中新增使用方法

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,7 @@
 let count = 0;
+const body = document.querySelector("body");
 
+// reset
 document.querySelector("#reset").addEventListener('click', () => {
   let input = document.querySelector("#inputSeq");
   let pyError = document.querySelectorAll(".py-error");
@@ -14,14 +16,37 @@ document.querySelector("#reset").addEventListener('click', () => {
   count = 0;
 });
 
+// submit
 document.querySelector("#submit").addEventListener('click', () => {
   count++;
   const newDiv = document.createElement('div');
   newDiv.id = `content-${count}`;
   newDiv.classList.add("content");
-  document.querySelector("body").appendChild(newDiv);
+  body.appendChild(newDiv);
   const order = document.createElement('div');
   order.classList.add("order");
   order.innerHTML = count;
   newDiv.appendChild(order);
 });
+
+// blastOnline
+document.querySelector('#blast').addEventListener('click', (e) => {
+  const inputSeq = document.querySelector("#inputSeq");
+  const frame = document.createElement("iframe");
+  const exitBtn = document.createElement("button");
+  frame.src = "https://blast.ncbi.nlm.nih.gov/Blast.cgi?PROGRAM=blastp&PAGE_TYPE=BlastSearch&LINK_LOC=blasthome";
+  frame.id = "blastOnline";
+  exitBtn.id = "closeWindow"
+  exitBtn.innerHTML = "Close Window";
+  inputSeq.appendChild(exitBtn);
+  inputSeq.appendChild(frame);
+  e.target.disabled = true;
+  e.target.style.color = "gray";
+  document.querySelector("#closeWindow").addEventListener('click', (e) => {
+    const blast = document.querySelector("#blast");
+    inputSeq.removeChild(frame);
+    blast.disabled = false;
+    inputSeq.removeChild(e.target);
+    blast.style.color = "white";
+  })
+})
